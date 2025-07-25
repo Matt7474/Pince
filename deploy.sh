b2826d0073d04c2dfe61c2d94d8b9a95fcb7792c
Matt7474 <dimier.matt@gmail.com>
2025-07-25 10:21:22 +0200

    Use SSH private key in deploy.sh for secure authentication

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,9 @@ REMOTE_USER="root"
 REMOTE_HOST="217.154.15.118"
 REMOTE_DIR="/root/pince-stack"
 
-tar czf $ARCHIVE_NAME back/ docker-compose.yml
-scp $ARCHIVE_NAME $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/
+scp -i ~/.ssh/id_rsa $ARCHIVE_NAME $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/
 
-ssh $REMOTE_USER@$REMOTE_HOST << EOF
+ssh -i ~/.ssh/id_rsa $REMOTE_USER@$REMOTE_HOST << EOF
   cd $REMOTE_DIR
   tar xzf $ARCHIVE_NAME
   docker system prune -a --volumes -f
@@ -16,3 +15,23 @@ ssh $REMOTE_USER@$REMOTE_HOST << EOF
   docker-compose build --no-cache
   docker-compose up -d
 EOF
+
+
+# #!/bin/bash
+
+# ARCHIVE_NAME="pince-stack.tar.gz"
+# REMOTE_USER="root"
+# REMOTE_HOST="217.154.15.118"
+# REMOTE_DIR="/root/pince-stack"
+
+# tar czf $ARCHIVE_NAME back/ docker-compose.yml
+# scp $ARCHIVE_NAME $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/
+
+# ssh $REMOTE_USER@$REMOTE_HOST << EOF
+#   cd $REMOTE_DIR
+#   tar xzf $ARCHIVE_NAME
+#   docker system prune -a --volumes -f
+#   docker-compose down --volumes
+#   docker-compose build --no-cache
+#   docker-compose up -d
+# EOF
