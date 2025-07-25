ab40c88677a1b84bb3c8eaa4c40512609658e4a1
Matt7474 <dimier.matt@gmail.com>
2025-07-25 10:11:49 +0200

    Include deploy.sh script in repository for GitHub Actions

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ARCHIVE_NAME="pince-stack.tar.gz"
+REMOTE_USER="root"
+REMOTE_HOST="217.154.15.118"
+REMOTE_DIR="/root/pince-stack"
+
+tar czf $ARCHIVE_NAME back/ docker-compose.yml
+scp $ARCHIVE_NAME $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/
+
+ssh $REMOTE_USER@$REMOTE_HOST << EOF
+  cd $REMOTE_DIR
+  tar xzf $ARCHIVE_NAME
+  docker system prune -a --volumes -f
+  docker-compose down --volumes
+  docker-compose build --no-cache
+  docker-compose up -d
+EOF
